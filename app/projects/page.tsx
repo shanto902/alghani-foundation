cc42bfdb1411d84c1299e5a390ce38dd2fc4e1a5
Ashik Ali Shanto <shanto902@gmail.com>
2025-02-09 22:28:53 +0600

    add projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+import HeaderBlock from "@/components/block/HeaderBlock";
+import ProjectSection from "@/components/section/ProjectSection";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <HeaderBlock title="Our Projects" />
+      <ProjectSection />
+    </div>
+  );
+};
+
+export default page;
---
+++ components/common/Card.tsx
@@ -0,0 +1,22 @@
+// components/Card.tsx
+import React from "react";
+
+interface CardProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+}
+
+const Card: React.FC<CardProps> = ({ imageUrl, title, description }) => {
+  return (
+    <div className="border rounded-lg overflow-hidden shadow-lg m-4 w-72">
+      <img src={imageUrl} alt={title} className="w-full h-48 object-cover" />
+      <div className="p-4">
+        <h3 className="font-semibold text-lg">{title}</h3>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
